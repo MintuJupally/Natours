18ca79231e28c2583b7513c13ffbaa9de32d33fc
Mintu Jupally <mintu.jupally@gmail.com>
2020-09-08 18:13:52 +0530

    checking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -52,7 +52,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
@@ -73,8 +72,6 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  console.log(event.data.object);
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
