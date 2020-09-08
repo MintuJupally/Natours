c6cb026399b1124292209cd24ec2ec47ee351b11
Mintu Jupally <mintu.jupally@gmail.com>
2020-09-08 11:26:53 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,17 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+app.use(cors());
+//Access-Control-Allow-Origin *
+
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   })
+// );//If api and fron tend application are on different domains
+
+app.options('*', cors());
+app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
