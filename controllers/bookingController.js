a88a019ad21397731de4f6c0e0b201aa11ad45be
Mintu Jupally <mintu.jupally@gmail.com>
2020-09-08 17:21:56 +0530

    Checking line items

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -52,6 +52,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
+  console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].amount / 100;
@@ -72,6 +73,8 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
+  console.log(event.data.object);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
